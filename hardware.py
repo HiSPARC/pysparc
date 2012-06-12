def2b6923f8d1e0a060e4074ce5c2e16484c4b32
David Fokkema <davidfokkema@gmail.com>
2012-06-12 15:54:44 +0200

    Refactored hardware code

---
+++ hardware.py
@@ -0,0 +1,31 @@
+import logging
+
+from pyftdi.pyftdi.ftdi import Ftdi
+from pyftdi.serialext.ftdiext import SerialFtdi
+
+logger = logging.getLogger(__name__)
+
+
+class Hardware:
+    def __init__(self):
+        logger.debug("Searching for HiSPARC III Master...")
+        master_url = self.get_master_url()
+        if not master_url:
+            raise RuntimeError("HiSPARC III Master not found")
+        logger.debug("Master found at %s" % master_url)
+        self.master = SerialFtdi(master_url, timeout=3)
+        self.init_hardware(self.master)
+        logger.info("HiSPARC III Master initialized")
+
+    def get_master_url(self):
+        devices = Ftdi.find_all([(0x403, 0x6010)])
+        for device in devices:
+            vendor_id, product_id, serial_str, num_interfaces, \
+                description = device
+            if description == "HiSPARC III Master":
+                return "ftdi://ftdi:2232:%s/2" % serial_str
+        return None
+
+    def init_hardware(self, device):
+        device.write('\x99\xff\x66')
+        device.write('\x99\x35\x00\x00\x00\x01\x66')
--- test_serial.py
+++ test_serial.py
@@ -1,24 +1,15 @@
-from pyftdi.pyftdi.ftdi import Ftdi
-from pyftdi.serialext.ftdiext import SerialFtdi
+import logging
+
+from hardware import Hardware
 
 
 def main():
-    master = find_master()
-    if not master:
-        raise RuntimeError("HiSPARC III Master not found")
-    s = SerialFtdi(master, timeout=3)
-    s.write('\x99\x35\x00\x00\x00\x03\x66')
-    while True:
-        msg = s.read(10000)
-        print repr(msg)
+    hardware = Hardware()
+    #while True:
+    #    msg = s.read(10000)
+    #    print repr(msg)
 
-def find_master():
-    devices = Ftdi.find_all([(0x403, 0x6010)])
-    for device in devices:
-        vendor_id, product_id, serial_str, num_interfaces, \
-            description = device 
-        if description == "HiSPARC III Master":
-            return "ftdi://ftdi:2232:%s/2" % serial_str
-    return None
 
-main()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
