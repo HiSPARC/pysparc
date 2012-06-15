58da9c3eada2289aa136768e8adcfcde2d824420
David Fokkema <davidfokkema@gmail.com>
2012-06-15 21:20:24 +0200

    Removed try..finally block from test

--- pysparc/test_hardware.py
+++ pysparc/test_hardware.py
@@ -11,19 +11,17 @@ def average(values):
     return sum(values) / len(values)
 
 def main():
-    try:
-        hardware = Hardware()
-        hardware.send_message(messages.TriggerConditionMessage(0x80))
-        hardware.send_message(messages.FullScaleAdjustHisparcMessage(0x80))
-        while True:
-            msg = hardware.read_message()
-            print msg
-            if type(msg) == messages.MeasuredDataMessage:
-                print average(msg.trace_ch1), msg.trace_ch1[:10]
-                print average(msg.trace_ch2), msg.trace_ch2[:10]
-                print len(msg.trace_ch1), len(msg.trace_ch2)
-    finally:
-        hardware.close()
+    hardware = Hardware()
+    hardware.send_message(messages.TriggerConditionMessage(0x80))
+    hardware.send_message(messages.FullScaleAdjustHisparcMessage(0x80))
+    while True:
+        msg = hardware.read_message()
+        print msg
+        if type(msg) == messages.MeasuredDataMessage:
+            print average(msg.trace_ch1), msg.trace_ch1[:10]
+            print average(msg.trace_ch2), msg.trace_ch2[:10]
+            print len(msg.trace_ch1), len(msg.trace_ch2)
+    hardware.close()
 
 
 if __name__ == '__main__':
