99b05b4ab4ec4f5e3bd889cb2f6a049ffa8fdc6a
David Fokkema <davidfokkema@gmail.com>
2014-01-14 21:35:06 +0100

    Test FTDI read timeout to be 16 ms
    On Linux, there is a strange bug that sets the timeout to zero.
    However, after sleep, with the FTDI device still plugged in *and
    used at least once*, the timeout kicks in and the effective
    timeout will be the 16 ms promised by a timeout inside the FTDI
    chip.
    
    Test that the timeout is 16 ms, not less.  We expect to not see
    data, so the PMT voltages must be zero!

--- pysparc/tests/muonlab/test_pylibftdi_hardware.py
+++ pysparc/tests/muonlab/test_pylibftdi_hardware.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 import pylibftdi
 
@@ -31,6 +32,26 @@ class OpenDeviceTest(unittest.TestCase):
 
     def test_open_device(self):
         device = pylibftdi.Device(DESCRIPTION)
+
+    def test_read_timeout(self):
+        # on Linux, there is a strange bug that sets the timeout to zero.
+        # However, after sleep, with the FTDI device still plugged in *and
+        # used at least once*, the timeout kicks in and the effective
+        # timeout will be the 16 ms promised by a timeout inside the FTDI
+        # chip.
+        #
+        # Test that the timeout is 16 ms, not less.  We expect to not see
+        # data, so the PMT voltages must be zero!
+
+        device = pylibftdi.Device(DESCRIPTION)
+        device.flush()
+        t = []
+        for i in range(10):
+            t0 = time.time()
+            device.read(62)
+            t.append(time.time() - t0)
+        mean_t = sum(t) / len(t)
+        self.assertAlmostEqual(mean_t, 16e-3, delta=2e-3)
 
 
 if __name__ == '__main__':
