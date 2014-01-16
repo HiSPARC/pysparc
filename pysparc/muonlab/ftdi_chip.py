4a44158efe149c285f945be9130e6b1810879a62
David Fokkema <davidfokkema@gmail.com>
2014-01-16 11:49:05 +0100

    Added docstrings (ftdi_chip now PEP 8 / 257 compliant)

--- pysparc/muonlab/ftdi_chip.py
+++ pysparc/muonlab/ftdi_chip.py
@@ -1,3 +1,28 @@
+"""Access FTDI hardware.
+
+Contents
+--------
+
+:class:`Error`
+    Base error class.
+
+:class:`DeviceNotFoundError`
+    Raised when device is not connected.
+
+:class:`DeviceError`
+    Raised for generic pylibftdi exceptions.
+
+:class:`ReadError`
+    Raised on read errors.
+
+:class:`WriteError`
+    Raised on write errors.
+
+:class:`FtdiChip`
+    Access FTDI hardware.
+
+"""
+
 import pylibftdi
 
 
@@ -11,11 +36,15 @@ BUFFER_SIZE = 64 * 62
 
 class Error(Exception):
 
+    """Base error class."""
+
     def __init__(self, msg):
         self.ftdi_msg = msg
 
 
 class DeviceNotFoundError(Error):
+
+    """Raised when device is not connected."""
 
     def __str__(self):
         return "Device not found."
@@ -23,11 +52,15 @@ class DeviceNotFoundError(Error):
 
 class DeviceError(Error):
 
+    """Raised for generic pylibftdi exceptions."""
+
     def __str__(self):
         return "Device error: %s" % self.ftdi_msg
 
 
 class ReadError(Error):
+
+    """Raised on read errors."""
 
     def __str__(self):
         return "Device read error: %s" % self.ftdi_msg
@@ -35,11 +68,25 @@ class ReadError(Error):
 
 class WriteError(Error):
 
+    """Raised on write errors."""
+
     def __str__(self):
         return "Device write error: %s" % self.ftdi_msg
 
 
 class FtdiChip(object):
+
+    """Access FTDI hardware.
+
+    Instantiate this class to get access to connected FTDI hardware.
+    The hardware device is opened during instantiation.
+
+    You can use the :meth:`find_all` static method to list all connected
+    devices before openening them::
+
+        >>> FtdiChip.find_all()
+
+    """
 
     _device = None
 
@@ -59,17 +106,39 @@ class FtdiChip(object):
             self.close()
 
     def close(self):
+        """Close device."""
+
         self._device.close()
 
     @staticmethod
     def find_all():
+        """Find all connected FTDI devices.
+
+        :returns: list of (manufacturer, description, serial#) tuples.
+
+        """
         return pylibftdi.Driver().list_devices()
 
     def flush_device(self):
+        """Flush device buffers.
+
+        To completely clear out outdated measurements, e.g. when changing
+        parameters, call this method.  All data received after this method
+        is called is really newly measured.
+
+        """
         self._device.flush()
         self._device.read(BUFFER_SIZE)
 
     def read(self):
+        """Read from device.
+
+        A read is tried three times.  When unsuccesful, raises
+        :class:`ReadError`.
+
+        :returns: string containing the data.
+
+        """
         for i in range(3):
             try:
                 data = self._device.read(READ_SIZE)
@@ -80,6 +149,14 @@ class FtdiChip(object):
         raise ReadError(str(exc))
 
     def write(self, data):
+        """Write to device.
+
+        A write is tried three times.  When unsuccesful, raises
+        :class:`WriteError`.
+
+        :param data: string containing the data to write.
+
+        """
         for i in range(3):
             try:
                 self._device.write(data)
