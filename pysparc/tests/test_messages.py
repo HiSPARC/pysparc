52ce1d5e173cb32b60ee664ff675599bc62f695a
David Fokkema <davidfokkema@gmail.com>
2014-07-15 20:59:58 +0200

    Fix tests that were broken by 165e9d9

--- pysparc/tests/test_messages.py
+++ pysparc/tests/test_messages.py
@@ -19,20 +19,20 @@ class HisparcMessageTest(unittest.TestCase):
         encoded_msg = self.msg.encode()
         self.assertEqual(encoded_msg, None)
 
-    @patch('pysparc.messages.Struct')
+    @patch('pysparc.messages.struct.Struct')
     def test_encode_uses_correct_format_if_no_msg_format(self, mock_Struct):
         self.msg.identifier = sentinel.identifier
         self.msg.encode()
         mock_Struct.assert_called_once_with('>BBB')
 
-    @patch('pysparc.messages.Struct')
+    @patch('pysparc.messages.struct.Struct')
     def test_encode_uses_correct_format_if_msg_format(self, mock_Struct):
         self.msg.identifier = sentinel.identifier
         self.msg.msg_format = 'FOO'
         self.msg.encode()
         mock_Struct.assert_called_once_with('>BBFOOB')
 
-    @patch('pysparc.messages.Struct')
+    @patch('pysparc.messages.struct.Struct')
     @patch.dict('pysparc.messages.codons', {'start': sentinel.start,
                                             'stop': sentinel.stop})
     def test_encode_calls_pack_with_codons(self, mock_Struct):
