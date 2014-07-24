96fd70515bc5aea21417f8f7a6f7d78b739f3e8b
David Fokkema <davidfokkema@gmail.com>
2014-07-24 22:43:33 +0200

    Refactored test_hv script to store data

--- scripts/test_hv.py
+++ scripts/test_hv.py
@@ -1,20 +1,41 @@
-from __future__ import division
-
 import logging
 import os
+
+import tables
 
 from pysparc.hardware import HiSPARCIII
 from pysparc.align_adcs import AlignADCs
 from pysparc import messages
 
 
-CONFIGFILE = 'config.ini'
+CONFIGFILE = os.path.expanduser('~/.pysparc')
+DATAFILE = 'hisparc.h5'
 
 
-def main():
-    hardware = HiSPARCIII()
+class HisparcEvent(tables.is_description):
+    event_id = tables.UInt32Col(pos=0)
+    timestamp = tables.Time32Col(pos=1)
+    nanoseconds = tables.UInt32Col(pos=2)
+    ext_timestamp = tables.UInt64Col(pos=3)
+    data_reduction = tables.BoolCol(pos=4)
+    trigger_pattern = tables.UInt32Col(pos=5)
+    baseline = tables.Int16Col(shape=4, dflt=-1, pos=6)
+    std_dev = tables.Int16Col(shape=4, dflt=-1, pos=7)
+    n_peaks = tables.Int16Col(shape=4, dflt=-1, pos=8)
+    pulseheights = tables.Int16Col(shape=4, dflt=-1, pos=9)
+    integrals = tables.Int32Col(shape=4, dflt=-1, pos=10)
+    traces = tables.Int32Col(shape=4, dflt=-1, pos=11)
+    event_rate = tables.Float32Col(pos=12)
 
-    try:
+
+class Main(object):
+
+    def __init__(self):
+        self.device = HiSPARCIII()
+        self.initialize_device()
+        self.initialize_local_storage()
+
+    def initialize_device(self):
         if not os.path.isfile(CONFIGFILE):
             logging.info("No config file found.  Aligning ADCs.")
             align_adcs = AlignADCs(hardware)
@@ -23,25 +44,66 @@ def main():
             logging.info("Reading config from file")
             hardware.config.read_config(CONFIGFILE)
 
-        hardware.config.ch1_voltage = 800
-        hardware.config.ch2_voltage = 800
+    def initialize_local_storage(self):
+        self.datafile = tables.open_file(DATAFILE, 'a')
+        if 'hisparc' in self.datafile:
+            raise RuntimeError("Sorry, I will not overwrite data.  Aborting.")
+        else:
+            self.events = self.datafile.create_table(
+                '/', 'hisparc', HisparcEvent)
+            self.blobs = self.datafile.create_vlarray(
+                '/', 'blobs', tables.VLStringAtom())
 
+    def run(self):
         # at least two low
-        hardware.config.trigger_condition = 0b10
-        hardware.send_message(messages.InitializeMessage(
+        self.device.config.trigger_condition = 0b10
+        self.device.send_message(messages.InitializeMessage(
                 one_second_enabled=True))
 
-        while True:
-            msg = hardware.read_message()
-            if msg is not None:
-                print msg
-    except KeyboardInterrupt:
-        print "Interrupted by user."
-    finally:
+        try:
+            while True:
+                msg = self.device.read_message()
+                if msg is not None:
+                    logging.info("Data received: %s", msg)
+                if isinstance(msg, MeasuredDataMessage):
+                    self.store_event(msg)
+        except KeyboardInterrupt:
+            logging.info("Interrupted by user.")
+
+    def store_event(self, event_msg):
+        row = self.events.row
+        row['event_id'] = len(self.events)
+        row['timestamp'] = msg.timestamp
+        row['nanoseconds'] = msg.nanoseconds
+        row['ext_timestamp'] = msg.timestamp * int(1e9) + msg.nanoseconds
+        row['data_reduction'] = False
+        row['trigger_pattern'] = msg.trigger_pattern
+        baselines = [msg.trace_ch1[:100].mean(),
+                     msg.trace_ch2[:100].mean(), -1, -1]
+        row['baselines'] = baselines
+        row['std_dev'] = [msg.trace_ch1[:100].std(),
+                          msg.trace_ch2[:100].std(), -1, -1]
+        row['n_peaks'] = 4 * [-999]
+        row['pulseheights'] = [msg.trace_ch1.max() - baseline[0],
+                               msg.trace_ch2.max() - baseline[1], -1, -1]
+        row['integrals'] = 4 * [-999]
+        row['traces'] = [len(self.blobs), len(self.blobs) + 1, -1, -1]
+        self.blobs.append(msg.trace_ch1)
+        self.blobs.append(msg.trace_ch2)
+        row['event_rate'] = -1
+
+        row.append()
+        self.events.flush()
+
+    def close(self):
         logging.info("Writing config to file")
-        hardware.config.write_config(CONFIGFILE)
+        self.device.config.write_config(CONFIGFILE)
+        self.device.close()
+        self.datafile.close()
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    main()
+    app = Main()
+    app.run()
+    app.close()
