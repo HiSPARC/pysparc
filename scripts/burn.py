51bfe4059e15d7d90a7b6e95f16a04110c2559a8
David Fokkema <davidfokkema@gmail.com>
2014-08-20 17:20:44 +0200

    Burn.py: reduce the need for bytearray()

--- scripts/burn.py
+++ scripts/burn.py
@@ -15,15 +15,10 @@ WRITE_BYTES_PVE_LSB = 0x18
 
 
 def write(dev, data):
-    dev.write(data)
-
-def print_low_bits(f):
-    write(f, bytearray([GET_BITS_LOW]))
-    time.sleep(.01)
-    print 'low: ', [bin(ord(u)) for u in f.read(1)]
+    dev.write(bytearray(data))
 
 def print_high_bits(f):
-    write(f, bytearray([GET_BITS_HIGH]))
+    write(f, [GET_BITS_HIGH])
     time.sleep(.01)
     print 'high:', [bin(ord(u)) for u in f.read(1)]
 
@@ -36,20 +31,20 @@ def burn(firmware_file):
     f.ftdi_fn.ftdi_set_bitmode(0, 0x02)
 
     # Set clock frequency to 30 MHz (0x0000)
-    write(f, bytearray([TCK_DIVISOR, 0, 0]))
+    write(f, [TCK_DIVISOR, 0, 0])
     # Disable divide clock frequency by 5
-    write(f, bytearray([DISABLE_CLK_DIV5]))
+    write(f, [DISABLE_CLK_DIV5])
 
     # bits 0 and 1 are output that is, bits TCK/SK and TDI/DO, clock and
     # data
-    write(f, bytearray([SET_BITS_LOW, 0, 0b11]))
+    write(f, [SET_BITS_LOW, 0, 0b11])
 
     print_high_bits(f)
     # pull nCONFIG (low byte bit 0) low
-    write(f, bytearray([SET_BITS_HIGH, 0, 1]))
+    write(f, [SET_BITS_HIGH, 0, 1])
     print_high_bits(f)
     # pull nCONFIG (low byte bit 0) high
-    write(f, bytearray([SET_BITS_HIGH, 1, 1]))
+    write(f, [SET_BITS_HIGH, 1, 1])
     print_high_bits(f)
 
     BUFSIZE = 64 * 1024
@@ -63,8 +58,8 @@ def burn(firmware_file):
             LENGTH = len(xbuf) - 1
             LENGTH_L = LENGTH & 0xff
             LENGTH_H = LENGTH >> 8 & 0xff
-            send_buf = bytearray([WRITE_BYTES_PVE_LSB, LENGTH_L, LENGTH_H]) + xbuf
-            write(f, send_buf)
+            write(f, [WRITE_BYTES_PVE_LSB, LENGTH_L, LENGTH_H])
+            write(f, xbuf)
 
     print_high_bits(f)
 
