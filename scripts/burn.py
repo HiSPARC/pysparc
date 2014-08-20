f0968f6295a9f31ea2b4e501bbe56f99a03ee67d
David Fokkema <davidfokkema@gmail.com>
2014-08-20 17:14:45 +0200

    Burn.py: use bytearray and remove cruft

--- scripts/burn.py
+++ scripts/burn.py
@@ -4,8 +4,6 @@ import sys
 
 import pylibftdi
 
-from array import array as Array
-
 
 SET_BITS_LOW = 0x80
 GET_BITS_LOW = 0x81
@@ -17,15 +15,15 @@ WRITE_BYTES_PVE_LSB = 0x18
 
 
 def write(dev, data):
-    dev.write(Array('B', data).tostring())
+    dev.write(data)
 
 def print_low_bits(f):
-    write(f, [GET_BITS_LOW])
+    write(f, bytearray([GET_BITS_LOW]))
     time.sleep(.01)
     print 'low: ', [bin(ord(u)) for u in f.read(1)]
 
 def print_high_bits(f):
-    write(f, [GET_BITS_HIGH])
+    write(f, bytearray([GET_BITS_HIGH]))
     time.sleep(.01)
     print 'high:', [bin(ord(u)) for u in f.read(1)]
 
@@ -37,25 +35,21 @@ def burn(firmware_file):
     # We'll set the direction explicitly later.
     f.ftdi_fn.ftdi_set_bitmode(0, 0x02)
 
-    # print_low_bits(f)
-
     # Set clock frequency to 30 MHz (0x0000)
-    write(f, [TCK_DIVISOR, 0, 0])
+    write(f, bytearray([TCK_DIVISOR, 0, 0]))
     # Disable divide clock frequency by 5
-    write(f, [DISABLE_CLK_DIV5])
-
-    # print_low_bits(f)
+    write(f, bytearray([DISABLE_CLK_DIV5]))
 
     # bits 0 and 1 are output that is, bits TCK/SK and TDI/DO, clock and
     # data
-    write(f, [SET_BITS_LOW, 0, 0b11])
+    write(f, bytearray([SET_BITS_LOW, 0, 0b11]))
 
     print_high_bits(f)
     # pull nCONFIG (low byte bit 0) low
-    write(f, [SET_BITS_HIGH, 0, 1])
+    write(f, bytearray([SET_BITS_HIGH, 0, 1]))
     print_high_bits(f)
     # pull nCONFIG (low byte bit 0) high
-    write(f, [SET_BITS_HIGH, 1, 1])
+    write(f, bytearray([SET_BITS_HIGH, 1, 1]))
     print_high_bits(f)
 
     BUFSIZE = 64 * 1024
@@ -69,15 +63,10 @@ def burn(firmware_file):
             LENGTH = len(xbuf) - 1
             LENGTH_L = LENGTH & 0xff
             LENGTH_H = LENGTH >> 8 & 0xff
-            send_buf = [WRITE_BYTES_PVE_LSB] + [LENGTH_L, LENGTH_H] + [ord(u) for u in xbuf]
+            send_buf = bytearray([WRITE_BYTES_PVE_LSB, LENGTH_L, LENGTH_H]) + xbuf
             write(f, send_buf)
 
-    #for i in range(10):
-    #    print_high_bits(f)
-    #    f.write([0x8e, 0])
-
     print_high_bits(f)
-    # print_low_bits(f)
 
 
 if len(sys.argv) < 2:
