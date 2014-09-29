b992de003d860c233ac62f0806aac72b6c1aa59a
David Fokkema <davidfokkema@gmail.com>
2014-09-29 16:40:01 +0200

    Test script: verify storage
    Keep track of all created and stored events, and make sure everything
    created is stored exactly once by both fake datastores.

--- scripts/test_storage_manager.py
+++ scripts/test_storage_manager.py
@@ -23,26 +23,68 @@ class FakeEvent(object):
 
 class FakeDataStore(storage.BaseDataStore):
 
+    def __init__(self):
+        super(FakeDataStore, self).__init__()
+        self._stored_timestamps = []
+
     def store_event(self, event):
+        time.sleep(random.uniform(0, .5))
         if random.random() < .1:
+            # Ohoh, problem!
+            time.sleep(random.uniform(2, 5))
             raise storage.StorageError("Random foo exception!")
+        else:
+            # Yes, succesful!
+            self._stored_timestamps.append(event.ext_timestamp)
+
+
+def create_event():
+    if random.random() < .5:
+        print "Event created."
+        event = FakeEvent()
+        manager.store_event(event)
+        time.sleep(random.uniform(0, .2))
+        return event.ext_timestamp
+    else:
+        return None
+
+
+def workers_store_event():
+    for queue, worker in manager.workers:
+        if random.random() < .5:
+            print queue, "Store event."
+            worker.store_event()
 
 
 def main():
+    global manager
+
     datastore1 = FakeDataStore()
+    datastore2 = FakeDataStore()
 
     manager = storage.StorageManager()
     manager.add_datastore(datastore1, 'queue1')
+    manager.add_datastore(datastore2, 'queue2')
 
-    while True:
-        if random.random() < .5:
-            print "Event created."
-            manager.store_event(FakeEvent())
-        if random.random() < .6:
-            for queue, worker in manager.workers:
-                print queue, "Store event."
-                worker.store_event()
-        time.sleep(.5)
+    created_timestamps = []
+
+    t0 = time.time()
+    while time.time() - t0 < 20:
+        ts = create_event()
+        if ts:
+            created_timestamps.append(ts)
+        workers_store_event()
+
+    t0 = time.time()
+    print "Storing backlog!!!"
+    while time.time() - t0 < 20:
+        workers_store_event()
+    st1 = datastore1._stored_timestamps
+    st2 = datastore2._stored_timestamps
+    print len(set(st1)), len(set(st2)), set(st1) == set(st2)
+    print len(st1), len(st2), st1 == st2
+
+    assert st1 == st2 == created_timestamps
 
 
 if __name__ == '__main__':
