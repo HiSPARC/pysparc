ab71d8423c063e2b90dd3ee9a5f1e85497633641
David Fokkema <davidfokkema@gmail.com>
2015-01-10 23:13:17 +0100

    Install OpenVPN at first boot

--- provisioning/provision_image.sh
+++ provisioning/provision_image.sh
@@ -5,12 +5,15 @@ USER_UID=1000
 USER_GID=1000
 MOUNT_POINT=/mnt
 IMG=/vagrant/pysparc.img
-DIR=$MOUNT_POINT/home/$USER_NAME
+HOME_DIR=$MOUNT_POINT/home/$USER_NAME
+OPENVPN_DIR=$MOUNT_POINT/etc/openvpn
 
+# Setup loopback interface for the image file
 sudo modprobe -r loop
 sudo modprobe loop max_part=63
 sudo losetup /dev/loop0 $IMG
 
+# If successful, mount the disk
 if [ -b /dev/loop0p2 ]
 then
   sudo mount /dev/loop0p2 $MOUNT_POINT
@@ -21,18 +24,24 @@ else
   exit 1
 fi
 
-if [ -d $DIR ]
+# If the home directory exists, add SSH keys
+if [ -d $HOME_DIR ]
 then
-  if [ ! -d $DIR/.ssh ]; then sudo mkdir $DIR/.ssh; fi
-  sudo cp /vagrant/provisioning/authorized_keys $DIR/.ssh
-  sudo chown -R 1000:1000 $DIR/.ssh
+  if [ ! -d $HOME_DIR/.ssh ]; then sudo mkdir $HOME_DIR/.ssh; fi
+  sudo cp /vagrant/provisioning/authorized_keys $HOME_DIR/.ssh
+  sudo chown -R 1000:1000 $HOME_DIR/.ssh
 else
   echo "Home directory could not be found in image."
 fi
 
-if [ ! -d /etc/openvpn ]; then sudo mkdir /etc/openvpn; fi
-sudo cp /vagrant/provisioning/hisparcvpn.conf /etc/openvpn
-sudo cp /vagrant/vpncert.zip /etc/openvpn
+# Copy the VPN config and certificates
+if [ ! -d $OPENVPN_DIR ]; then sudo mkdir $OPENVPN_DIR; fi
+sudo cp /vagrant/provisioning/hisparcvpn.conf $OPENVPN_DIR
+sudo cp /vagrant/vpncert.zip $OPENVPN_DIR
 
+# Setup the first boot script
+sudo cp --preserve=mode /vagrant/provisioning/firstboot.sh $MOUNT_POINT/etc/rc.local
+
+# Unmount the disk
 sudo umount $MOUNT_POINT
 sudo losetup -d /dev/loop0
