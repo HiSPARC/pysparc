ba8f841db28569bee01368644258504ad6c921b4
David Fokkema <davidfokkema@gmail.com>
2015-03-18 21:18:20 +0100

    Add GPSMessageFactory
    I know, I know, a lot of duplication from messages.py! However, it may
    require a deep-level hack to generalize the code. Or just a thorough
    rewrite of the factory implementation. For now, this will do.

--- pysparc/gps_messages.py
+++ pysparc/gps_messages.py
@@ -45,3 +45,86 @@ class GPSMessage(HisparcMessage):
 
     """
     container_format = '>BB%sH'
+
+
+def GPSMessageFactory(buff):
+    """Return a message, extracted from the buffer
+
+    Inspect the buffer and extract the first full message. A
+    GPSMessage subclass instance will be returned, according to
+    the type of the message.
+
+    :param buff: the contents of the usb buffer
+    :return: instance of a GPSMessage subclass
+
+    """
+    while True:
+        try:
+            GPSMessage.validate_message_start(buff)
+        except StartCodonError:
+            logger.warning("Start codon error, stripping buffer.")
+            strip_until_start_codon(buff)
+        except IndexError:
+            return None
+        else:
+            break
+
+    for cls in GPSMessage.__subclasses__():
+        if cls.is_message_for(buff):
+            try:
+                return cls(buff)
+            except CorruptMessageError:
+                logger.warning("Corrupt message, stripping buffer.")
+                strip_partial_message(buff)
+                return GPSMessageFactory(buff)
+            except struct.error:
+                # message is too short, wait for the rest to come in.
+                logger.debug("Message is too short, wait for more data.")
+                return None
+            except ValueError:
+                # some value in a message could not be converted.
+                # Probably a corrupt message
+                logger.warning("ValueError, so probably a corrupt message; "
+                               "stripping buffer.")
+                strip_partial_message(buff)
+                return GPSMessageFactory(buff)
+
+    # Unknown message type.  This usually happens after a partial or
+    # corrupt message is stripped away until a new start codon is found.
+    # This 'start codon' is probably not an actual start codon, but
+    # somewhere in the middle of a partial message.
+    logger.warning("Unknown message type (probably corrupt), "
+                   "stripping buffer.")
+    strip_partial_message(buff)
+    return GPSMessageFactory(buff)
+
+
+def strip_until_start_codon(buff):
+    """Strip bytes from left until start codon is found.
+
+    This method assumes that the data at the start of the buffer is
+    actually somewhere in the middle of a message.
+
+    :param buff: the contents of the usb buffer
+
+    """
+    try:
+        index = buff.index(chr(codons['start']))
+    except ValueError:
+        del buff[:]
+    else:
+        del buff[:index]
+
+
+def strip_partial_message(buff):
+    """Strip partial message from left, until new start codon found.
+
+    This method assumes that there is a message in the buffer, but that
+    it is only a partial message.  Strip data until a new message appears
+    to begin.
+
+    :param buff: the contents of the usb buffer
+
+    """
+    del buff[0]
+    return strip_until_start_codon(buff)
