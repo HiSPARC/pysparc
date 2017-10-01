a4da526e764170156886059f09a0dea2d6e3942b
David Fokkema <davidfokkema@gmail.com>
2017-10-01 21:09:11 +0200

    Test alignment for master and slave
    Fails for slave

--- scripts/test_master_slave.py
+++ scripts/test_master_slave.py
@@ -1,5 +1,25 @@
 from pysparc.hardware import HiSPARCIII
+from pysparc.align_adcs import AlignADCs
+
 
 if __name__ == '__main__':
     master = HiSPARCIII()
     slave = HiSPARCIII(slave=True)
+
+    align_adcs = AlignADCs(master)
+    align_adcs.align()
+    print
+    print "All configuration settings:"
+    print
+    for attr in sorted(master.config.members()):
+        print attr, getattr(master.config, attr)
+    print
+
+    align_adcs = AlignADCs(slave)
+    align_adcs.align()
+    print
+    print "All configuration settings:"
+    print
+    for attr in sorted(slave.config.members()):
+        print attr, getattr(slave.config, attr)
+    print
