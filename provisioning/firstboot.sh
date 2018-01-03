9902c3280cb87ebe7f4144f56f10b8d9ed16b028
David Fokkema <davidfokkema@gmail.com>
2018-01-03 21:28:00 +0100

    Fix systemd funniness and unzip run-more-than-once problem

--- provisioning/firstboot.sh
+++ provisioning/firstboot.sh
@@ -11,10 +11,14 @@
 #
 # By default this script does nothing.
 
+# Systemd executes this script after the network is up. However, it still needs
+# a bit more time, requiring this very ugly hack.
+sleep 10
+
 # Install OpenVPN
 apt-get update
 yes | apt-get install unzip openvpn
-unzip /etc/openvpn/vpncert.zip -d /etc/openvpn
+yes | unzip /etc/openvpn/vpncert.zip -d /etc/openvpn
 service openvpn restart
 
 exit 0
